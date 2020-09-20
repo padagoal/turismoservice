55413daa6bcbcbca8a0fc548836192120fc8aba1
Pablo Gonzalez <padagoal012@gmail.com>
2020-09-20 01:35:07 -0400

    Add Inline Config for PhotoRooms

--- TurismoHotel/admin.py
+++ TurismoHotel/admin.py
@@ -8,6 +8,16 @@ admin.site.register(ServiceRoom)
 
 
 admin.site.register(Hotel)
-admin.site.register(Rooms)
+#admin.site.register(Rooms)
+
+class PhotosRoomInline(admin.TabularInline):
+    model = PhotosRoom
 
 admin.site.register(PhotosRoom)
+
+class RoomAdmin(admin.ModelAdmin):
+    inlines = [
+        PhotosRoomInline,
+    ]
+
+admin.site.register(Rooms,RoomAdmin)
